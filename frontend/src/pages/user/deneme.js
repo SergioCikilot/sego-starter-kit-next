20d15b5d0bf05fb08bc176fcf8377976d43e8c2f
SergioCikilot <62554373+SergioCikilot@users.noreply.github.com>
2022-08-14 15:12:46 +0300

    init

---
+++ frontend/src/pages/user/deneme.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+const deneme = () => {
+  return <div className={styles.main}>deneme</div>;
+};
+
+export default deneme;
